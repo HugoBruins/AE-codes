feff9c4ea9509d4d8b16b9a03e6e02b1782f26ec
Hugo Bruins <hugobruins@gmail.com>
2021-09-10 12:24:40 +0200

    Fixed a bug that made last step inaccurate

--- BSC-1/Q1/Intro AE I/c++/Standard Atmosphere Calculator/src.cpp
+++ BSC-1/Q1/Intro AE I/c++/Standard Atmosphere Calculator/src.cpp
@@ -3,27 +3,27 @@
 #include <cmath>
 
 // Function to calculate the pressure with given values
-double calculatePressure(const double& p_0, const double& T_0, const double& T_1, const double& a, const double& h_1, const double& h_0) 
+double calculatePressure(const double& p_0, const double& T_0, const double& T_1, const double& a, const double& h_1, const double& h_0)
 {
     double p_1;
-    if (a != 0) 
-        p_1 = p_0 * pow(T_1 / T_0, (-9.80665) / (a * 287)); 
-    else 
-        p_1 = p_0 * exp((-9.80665 * (h_1 - h_0) )/(287*T_0));
+    if (a != 0)
+        p_1 = p_0 * pow(T_1 / T_0, (-9.80665) / (a * 287));
+    else
+        p_1 = p_0 * exp((-9.80665 * (h_1 - h_0)) / (287 * T_0));
     std::cout << "Lapse rate:         " << a << " (K*m^-1)\n";
     std::cout << "Altitude Change:    " << h_0 << " -> " << h_1 << " (m)\n";
-    std::cout << "Temperature Change: " << T_0 - 273.15 << " -> " << T_1 - 273.15<< " (C)\n";
+    std::cout << "Temperature Change: " << T_0 - 273.15 << " -> " << T_1 - 273.15 << " (C)\n";
     std::cout << "Pressure Change:    " << p_0 << " -> " << p_1 << " (N*m^-2); Percentage of sea level: " << (p_1 * 100) / 101325 << " %\n";
     std::cout << "Density Change:     " << p_0 / (287 * T_0) << " -> " << p_1 / (287 * T_1) << " (kg*m^-3)\n\n";
     return p_1;
 }
 
 // The main juice
-double calculateNextLayer(const double& maxAltitude, const double& p_0 = 101325, const double& h_0 = 0, const double& T_0 = 288.15, const int& iteration = 0) 
+double calculateNextLayer(const double& maxAltitude, const double& p_0 = 101325, const double& h_0 = 0, const double& T_0 = 288.15, const int& iteration = 0)
 {
     // {Altitude, Lapse rate (Kelvin per meter) for below that altitude}
-    double milePoints[8][2] = 
-    { 
+    double milePoints[8][2] =
+    {
         {11000.0,   -0.0065},
         {20000.0,    0.0},
         {32000.0,    0.001},
@@ -31,19 +31,22 @@ double calculateNextLayer(const double& maxAltitude, const double& p_0 = 101325,
         {51000.0,    0},
         {71000.0,   -0.0028},
         {84852.0,   -0.002},
-        {10E304 ,    0} 
+        {10E304 ,    0}
     };
     double a = milePoints[iteration][1];
     double h_1 = milePoints[iteration][0];
-    double T_1 = T_0 + a * (h_1 - h_0);
-
-    if (maxAltitude > milePoints[iteration][0]) 
+    
+    if (maxAltitude > milePoints[iteration][0])
     {
+        double T_1 = T_1 = T_0 + a * (h_1 - h_0);
         double p_1 = calculatePressure(p_0, T_0, T_1, a, h_1, h_0);
         return calculateNextLayer(maxAltitude, p_1, h_1, T_1, iteration + 1);
     }
     else
-        return calculatePressure(p_0, T_0, T_1, a, maxAltitude, h_0);;
+    {
+        double T_1 = T_0 + a * (maxAltitude - h_0);
+        return calculatePressure(p_0, T_0, T_1, a, maxAltitude, h_0);
+    }
 }
 
 int main()
